d84a3a52f2c90ef9597a62c2ebb66f4d635a26de
Gessé Carlos <gesse.dev@outlook.com>
2021-12-28 14:30:38 -0300

    i changed mysql lib to mysql2 to avoid errors and create a update and delete methods to database

--- controllers/calls.js
+++ controllers/calls.js
@@ -1,13 +1,30 @@
 const Call = require('../models/calls');
 
 module.exports = (app) => {
-  app.get('/calls',
-  (req, res) => res.send('You\'re in calls route and running a GET method!'));
+  app.get('/calls', (req, res) => Call.read(res));
+
+  app.get('/calls/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    Call.queryById(id, res);
+  })
 
   app.post('/calls', (req, res) => {
     const call = req.body;
 
-    Call.create(call);
-    res.send('You\'re in calls route and running a POST method')
+    Call.create(call, res);
+  });
+
+  app.patch('/atendimentos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const values = req.body;
+
+    Call.update(id, values, res);
+  });
+
+  app.delete('/atendientos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    Call.delete(id, res);
   });
 };
--- models/calls.js
+++ models/calls.js
@@ -1,17 +1,99 @@
+const moment = require('moment');
 const connection = require('../utils/connection');
 
 class Call {
-  create(call) {
-    const sql = 'INSERT INTO Calls SET ?';
+  create(call, res) {
+    const creation_date = moment(call.creation_date, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+    const scheduled_date = moment(call.scheduled_date, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
 
-    connection.query(sql, call, (error, result) => {
+    const validateData = moment(scheduled_date).isSameOrAfter(creation_date);
+    const validateClient = call.client.length >= 2;
+
+    const validations = [
+      {
+        name: 'date',
+        valid: validateData,
+        message: 'The input date must be same or after the creation date',
+      },
+      {
+        name: 'client',
+        valid: validateClient,
+        message: 'Client name must be at least 2 characters',
+      },
+    ];
+
+    const errors = validations.filter((validation) => !validation.valid);
+
+    try {
+      const callDate = {...call, scheduled_date, creation_date};
+
+      const sql = 'INSERT INTO Calls SET ?';
+
+      connection.query(sql, callDate, (error, result) => {
+        if (error) {
+          res.status(400).json(error);
+        } else {
+          res.status(201).json(call);
+        }
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(400).json(errors);
+    }
+  };
+
+  read(res) {
+    const sql = 'SELECT * FROM Calls';
+
+    connection.query(sql, (error, results) => {
       if (error) {
-        console.log(error);
+        res.status(400).json(error);
       } else {
-        console.log(result);
+        res.status(200).json(results);
       }
     })
-  }
+  };
+
+  queryById(id, res) {
+    const sql = `SELECT * FROM Calls WHERE id=${id}`;
+
+    connection.query(sql, (error, results) => {
+      const result = results[0];
+      if (error) {
+        res.status(400).json(error);
+      } else {
+        res.status(200).json(result);
+      }
+    });
+  };
+
+  update(id, values, res) {
+    if (values.date) {
+      values.date = moment(values.date, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+    }
+
+    const sql = 'UPDATE Calls SET ? WHERE id=?';
+
+    connection.query(sql, [values, id], (error, result) => {
+      if (error) {
+        res.status(400).json(error);
+      } else {
+        res.status(200).json({...values, id});
+      }
+    });
+  };
+
+  delete(id, res) {
+    const sql = 'DELETE FROM Calls WHERE id=?';
+
+    connection.query(sql, id, (error, result) => {
+      if (error) {
+        res.status(400).json(error);
+      } else {
+        res.status(200).json({ id });
+      }
+    });
+  };
 }
 
 module.exports = new Call;
